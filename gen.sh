45e966a2a874c645de2589c1243037a610085d84
Yousong Zhou <yszhou4tech@gmail.com>
2014-02-25 18:03:37 +0800

    gen.sh: prepare built-binaries/ and fix filenames.

--- gen.sh
+++ gen.sh
@@ -3,11 +3,14 @@
 GOOS="windows linux"
 GOARCH="386 amd64 arm"
 
+mkdir -p built-binaries/
+rm -f built-binaries/*
 for os in $GOOS ; do
 	for arch in $GOARCH ; do
 		if [ "$arch" = "arm" -a "$os" = "windows" ] ; then
 			continue
 		fi
+		suffix=""
 		[ "$os" = "windows" ] && suffix=".exe"
 		oname="built-binaries/hstats-$os-$arch$suffix"
 		GOOS="$os" GOARCH="$arch" go build -ldflags="-s -w" -o "$oname" hstats.go
