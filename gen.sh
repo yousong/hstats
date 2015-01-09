272386066c175493a93e9f14a0a1ede2d35fe374
Yousong Zhou <yszhou4tech@gmail.com>
2015-01-10 00:18:39 +0800

    gen.sh: rework for darwin binaries generation.

--- gen.sh
+++ gen.sh
@@ -1,18 +1,44 @@
 #!/bin/sh
 
-GOOS="windows linux"
-GOARCH="386 amd64 arm"
+prepare() {
+	mkdir -p built-binaries/
+	rm -f built-binaries/*
+}
 
-mkdir -p built-binaries/
-rm -f built-binaries/*
-for os in $GOOS ; do
-	for arch in $GOARCH ; do
-		if [ "$arch" = "arm" -a "$os" = "windows" ] ; then
-			continue
-		fi
-		suffix=""
-		[ "$os" = "windows" ] && suffix=".exe"
-		oname="built-binaries/hstats-$os-$arch$suffix"
-		GOOS="$os" GOARCH="$arch" go build -ldflags="-s -w" -o "$oname" hstats.go
+buildhstats() {
+	local os="$1"
+	local arch="$2"
+	local suffix oname
+
+	[ "$os" = "windows" ] && suffix=".exe"
+	oname="built-binaries/hstats-$os-$arch$suffix"
+	GOOS="$os" GOARCH="$arch" \
+		go build -ldflags="-s -w" -o "$oname" hstats.go
+}
+
+gocrossloop() {
+	local goos="windows linux darwin"
+	local goarch="386 amd64 arm"
+
+	for os in $goos ; do
+		for arch in $goarch ; do
+			[ "$arch" = "arm" -a "$os" = "windows" ] && continue
+			[ "$arch" = "arm" -a "$os" = "darwin" ] && continue
+
+			"$@" "$os" "$arch"
+		done
 	done
-done
+}
+
+hstats() {
+	prepare
+	gocrossloop buildhstats
+}
+
+# crossing hstats needs crossing go itself first.
+crossgo() {
+	gocrossloop ./make.bash --no-clean
+}
+
+# cross, then
+"${1:-hstats}"
